f35700fa384576d447bfc7ba5e24fee6075075aa
Rexqwer911 <rexqwer911@gmail.com>
2024-12-05 02:25:51 +0300

    docx2pdf v2.0 Added byte conversion .gitignore

--- docxtopdf.py
+++ docxtopdf.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 UPLOAD_FOLDER = '/tmp/uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-@app.route('/convert', methods=['POST'])
+@app.route('/convert/form-data', methods=['POST'])
 def convert_docx_to_pdf():
 
     if 'file' not in request.files:
@@ -27,7 +27,6 @@ def convert_docx_to_pdf():
     filename = str(file.filename)
 
     input_path = os.path.join(uuid_folder_path, filename)
-    print(str(input_path))
     file.save(input_path)
 
     try:
@@ -42,6 +41,36 @@ def convert_docx_to_pdf():
     shutil.rmtree(uuid_folder_path)
 
     return Response(pdf_data, mimetype='application/pdf', headers={'Content-Disposition': f'attachment; filename={pdf_filename}'})
+
+@app.route('/convert/bytes', methods=['POST'])
+def convert_docx_bytes_to_pdf():
+
+    if not request.data:
+        return "No data provided", 400
+
+    unique_folder_name = str(uuid.uuid4())
+    uuid_folder_path = os.path.join(UPLOAD_FOLDER, unique_folder_name)
+    os.makedirs(uuid_folder_path, exist_ok=True)
+
+    input_path = os.path.join(uuid_folder_path, 'input.docx')
+    output_path = os.path.join(uuid_folder_path, 'input.pdf')
+
+    try:
+        with open(input_path, 'wb') as f:
+            f.write(request.data)
+
+        subprocess.run(['libreoffice', '--headless', '--convert-to', 'pdf', input_path, '--outdir', uuid_folder_path], check=True)
+
+        with open(output_path, 'rb') as pdf_file:
+            pdf_data = pdf_file.read()
+
+        return Response(pdf_data, mimetype='application/pdf')
+
+    except subprocess.CalledProcessError as e:
+        return f"Error during conversion: {e}", 500
+
+    finally:
+        shutil.rmtree(uuid_folder_path)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
